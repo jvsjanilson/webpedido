572a7986794b9d7f030d19a683dfdfea17441430
Janilson Varela <janilsonjvs@gmail.com>
2022-12-20 09:31:12 -0300

    feat: modeladmin estado e cidade

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import Estado, Cidade
 
-# Register your models here.
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('uf', 'nome')
+    fields = ('uf', 'nome')
+
+
+@admin.register(Cidade)
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'estado')
+    fields = ('nome', 'estado')
+
