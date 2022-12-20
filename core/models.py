474f75fe557f77ded66f8250892252e48d287206
Janilson Varela <janilsonjvs@gmail.com>
2022-12-20 09:30:59 -0300

    feat: model estado e cidade

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Estado(models.Model):
+    uf = models.CharField(max_length=2)
+    nome = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.uf
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=60)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.nome
+
