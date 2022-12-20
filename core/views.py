2564030b4ca5e9cc386d15cf496716e60cbf977e
Janilson Varela <janilsonjvs@gmail.com>
2022-12-20 09:30:48 -0300

    feat: views estado e cidade

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from core.models import Estado, Cidade
+from core.serializers import EstadoSerializer, CidadeSerializer
+
+
+class EstadoAPIView(APIView):
+    """
+        API Estado
+    """
+
+    def get(self, request):
+        estados = Estado.objects.all()
+        serializer = EstadoSerializer(estados, many=True)
+        return Response(serializer.data)
+
+
+class CidadeAPIView(APIView):
+    """
+        API Cidade
+    """
+
+    def get(self, request):
+        cidades = Cidade.objects.all()
+        serializer = CidadeSerializer(cidades, many=True)
+        return Response(serializer.data)
+
